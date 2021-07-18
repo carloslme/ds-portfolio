dbed032df9f56cdaee4e2de4ea49fe25628a21b5
carloslme <carloslmescom@gmail.com>
2021-07-18 00:20:13 -0500

    Add data dictionaries

---
+++ predictive-analytics/dictionaries.py
@@ -0,0 +1,28 @@
+days = {
+    'Domingo': 0,
+    'Jueves': 1,
+    'Lunes': 2,
+    'Martes': 3,
+    'Miercoles': 4,
+    'Sabado': 5,
+    'Viernes': 6,
+}
+
+towns = {
+    'ALVARO OBREGON': 0, 
+    'AZCAPOTZALCO': 1,
+    'BENITO JUAREZ': 2, 
+    'COYOACAN': 3,
+    'CUAJIMALPA': 4, 
+    'CUAUHTEMOC': 5, 
+    'GUSTAVO A. MADERO': 6, 
+    'IZTACALCO': 7,
+    'IZTAPALAPA': 8, 
+    'MAGDALENA CONTRERAS': 9, 
+    'MIGUEL HIDALGO': 10,
+    'MILPA ALTA': 11, 
+    'TLAHUAC': 12, 
+    'TLALPAN': 13, 
+    'VENUSTIANO CARRANZA': 14,
+    'XOCHIMILCO': 15
+}
