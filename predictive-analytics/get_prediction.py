daf6b87e30580c46db12aa8569a27b833148c904
carloslme <carloslmescom@gmail.com>
2021-07-20 23:33:07 -0500

    Modify model joblib path

--- predictive-analytics/get_prediction.py
+++ predictive-analytics/get_prediction.py
@@ -4,13 +4,11 @@ import numpy as np
 import transformers as t
 
 # load the pipeline object1
-model = load("D:\Cursos\ds-portfolio\ds-portfolio\predictive-analytics\ciclist-accident-classification.joblib")
+model = load("predictive-analytics\ciclist-accident-classification.joblib")
 
 
 def requestResults(data):
-    # [latitud, longitud, dia_semana, clas_con_f_alarma, tipo_entrada, hora_creacion]
     # [latitud, longitud, dia_semana, delegacion_inicio, hora_creacion]
-    values = np.array([19.47, -99.12, 1.0, 19.0])
     prediction = model.predict(np.array(data).reshape(1, -1))
     return prediction
 
